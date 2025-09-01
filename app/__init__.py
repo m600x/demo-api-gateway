d6d2d1ca3af21be7b0791536cb65f94c4a6cedc5
M600 <contact@m600.fr>
2025-09-01 10:11:04 +0200

    feat: Add log output endpoint

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,25 @@
 import os
 import logging
-from flask import Flask, jsonify, g
+import time
+from datetime import datetime
+from flask import Flask, request, jsonify, g, has_request_context, make_response
 
+'''
+Custom logging filter to populate dynamic field per request (remote_addr)
+'''
+class CustomLoggingFilter(logging.Filter):
+    def filter(self, record):
+        if has_request_context():
+            record.source_ip = getattr(g, "source_ip", "NA")
+        else:
+            record.source_ip = "-"
+        return True
 
 def create_app() -> Flask:
     app = Flask(__name__)
 
     logging.getLogger("werkzeug").setLevel(logging.ERROR)
-    logFormatter = logging.Formatter("[%(asctime)s] [%(levelname)s] %(message)s")
+    logFormatter = logging.Formatter("[%(asctime)s] [%(levelname)s] [%(source_ip)s] %(message)s")
     os.makedirs("/logs", exist_ok=True)
     root = logging.getLogger()
     root.handlers.clear()
@@ -16,21 +28,45 @@ def create_app() -> Flask:
     # Console logger to keep stdout stream
     consoleHandler = logging.StreamHandler()
     consoleHandler.setFormatter(logFormatter)
+    consoleHandler.addFilter(CustomLoggingFilter())
     root.addHandler(consoleHandler)
 
-    app.logger.warning("Ayoooo")
+    # File logger
+    fileHandler = logging.FileHandler("/logs/demo.log")
+    fileHandler.setFormatter(logFormatter)
+    root.addHandler(fileHandler)
+
+    app.logger.warning("The app is a demo, no proper WSGI is implemented since it's not required")
 
     @app.before_request
     def before():
+        g.source_ip = request.remote_addr
+        g.start_time = time.time() * 1000
+        g.timestamp = datetime.utcnow().isoformat() + "Z"
         app.logger.info("Incoming request")
 
     @app.after_request
     def after(resp):
+        latency = int(round((time.time() * 1000) - g.start_time))
+        app.logger.info("Processing ended with a latency of %dms", latency)
         return resp
 
     @app.get("/")
     def home():
         return jsonify({"status": "ok", "time": g.timestamp}), 200
+
+    @app.get("/logs")
+    def logs():
+        try:
+            with open('/logs/demo.log', 'r') as file:
+                logs = file.read()
+            resp = make_response(logs, 200)
+            resp.mimetype = "text/plain"
+            app.logger.info("Returning app logs")
+            return resp
+        except Exception as e:
+            app.logger.error("Error while reading log file: %s", e)
+        return jsonify({"error": "Cannot access log file"}), 500
 
     return app
 
